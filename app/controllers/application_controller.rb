c1f7212870bd71ef35f06ae32a27fd2bcd8ad5bb
Dan Varela <vareladanmarj@gmail.com>
2024-06-26 21:27:18 +0800

    feat: handle record not found errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
   private
+
+  def render_not_found
+    render json: {errors: "resource not found"}, status: :not_found
+  end
 
   def authenticate
     rodauth.require_account
