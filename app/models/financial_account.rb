57d0c073513e14e6f8eb5d756dc46bbeca3737a5
Dan Varela <vareladanmarj@gmail.com>
2024-06-28 01:04:36 +0800

    feat: make financial account name to be unique per account

--- app/models/financial_account.rb
+++ app/models/financial_account.rb
@@ -1,6 +1,6 @@
 class FinancialAccount < ApplicationRecord
   belongs_to :account
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: {scope: :account_id, case_sensitive: false}
   validates :amount, presence: true
 end
