036e071838d1ba4bf8b2a79a3bb626998afa6a1a
Dan Varela <vareladanmarj@gmail.com>
2024-06-28 05:17:52 +0800

    fix(category_spec): change to the correct factory

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.describe Category, type: :model do
   context "validations" do
-    subject { build(:account) }
+    subject { build(:category) }
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).scoped_to(:account_id).case_insensitive }
     it { should validate_presence_of(:category_type) }
   end
 
   context "associations" do
-    subject { build(:account) }
+    subject { build(:category) }
     it { should belong_to(:account) }
   end
 end
