6a4635179c844988904d898653ccd4970ab1000a
Dan Varela <vareladanmarj@gmail.com>
2024-06-28 22:30:20 +0800

    feat(expenses): validate that the associated category is of expense type

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,15 @@
 class Expense < ApplicationRecord
+  validate :associated_category_must_be_expense
+
   belongs_to :account
   belongs_to :category
   belongs_to :financial_account
 
   validates :date, presence: true
+
+  def associated_category_must_be_expense
+    unless category.expense?
+      errors.add(:category_id, "must be an expense category type")
+    end
+  end
 end
