05a2cb7ea50ea89e2204f111f617085ecb01be60
Dan Varela <vareladanmarj@gmail.com>
2024-06-30 04:10:01 +0800

    refactor: use hash instead of array in declaring enum

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :transactions, dependent: :nullify
   belongs_to :account
-  enum :category_type, [:expense, :income]
+  enum :category_type, expense: 1, income: 2
   validates :name, presence: true, uniqueness: {scope: :account_id, case_sensitive: false}
   validates :category_type, presence: true
 end
