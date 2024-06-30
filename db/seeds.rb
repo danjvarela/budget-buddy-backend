f2c40bdbf428a25f3781efc14dcd75f0e5cb2787
Dan Varela <vareladanmarj@gmail.com>
2024-07-01 05:57:59 +0800

    feat: seed database with transactions

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,53 @@
 #   end
 
 def generate_records_for(user)
-  ["Household", "Transportation", "Food", "Health", "Social Life", "Others"].each do |name|
-    user.categories.create name: name, category_type: "expense"
+  expense_categories = ["Household", "Transportation", "Food", "Health", "Social Life", "Others"].map do |name|
+    user.categories.create! name: name, category_type: "expense"
   end
 
-  ["Salary", "Petty Cash", "Allowance", "Others"].each do |name|
-    user.categories.create name: name, category_type: "income"
+  income_categories = ["Salary", "Petty Cash", "Allowance", "Others"].map do |name|
+    user.categories.create! name: name, category_type: "income"
   end
 
-  ["Cash", "Savings Account", "Digital Wallet"].each do |name|
-    user.financial_accounts.create name: name
+  financial_accounts = ["Cash", "Savings Account", "Digital Wallet"].map do |name|
+    user.financial_accounts.create! name: name
   end
 
-  user.transactions.create transaction_type: "expense", amount: 100, description: "Test Transaction", category: user.categories.expense.first, financial_account: user.financial_accounts.first, date: DateTime.now
-  user.transactions.create transaction_type: "income", amount: 100, description: "Test Transaction", category: user.categories.income.first, financial_account: user.financial_accounts.first, date: DateTime.now
-  user.transactions.create transaction_type: "transfer", amount: 100, description: "Test Transaction", from_financial_account: user.financial_accounts.first, to_financial_account: user.financial_accounts.second, date: DateTime.now
+  100.times do |n|
+    user.transactions.create({
+      financial_account: financial_accounts.sample,
+      category: expense_categories.sample,
+      description: "Expense Transaction #{n}",
+      amount: [100, 200, 300, 500, 1000].sample,
+      transaction_type: "expense",
+      date: DateTime.now - n.days
+    })
+  end
+  puts "Generated expense transactions for #{user.email}"
+
+  10.times do |n|
+    user.transactions.create({
+      financial_account: financial_accounts.sample,
+      category: income_categories.sample,
+      description: "Income Transaction #{n}",
+      amount: [35000, 10000, 20000].sample,
+      transaction_type: "income",
+      date: DateTime.now - n.days
+    })
+  end
+  puts "Generated income transactions for #{user.email}"
+
+  5.times do |n|
+    user.transactions.create({
+      from_financial_account: financial_accounts.first,
+      to_financial_account: financial_accounts.second,
+      description: "Transaction #{n}",
+      amount: [100, 200, 300].sample,
+      transaction_type: "transfer",
+      date: DateTime.now - n.days
+    })
+  end
+  puts "Generated transfer transactions for #{user.email}"
 end
 
 user = User.create!(email: "example@email.com", password: "123qwe123")
