6e1ed2d1a53b359c17a6853010c5ae0e17e7da32
Dan Varela <vareladanmarj@gmail.com>
2024-07-01 10:21:11 +0800

    feat: add deployed frontend urls to cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "http://127.0.0.1:3001"
+    origins "http://localhost:3001", "http://127.0.0.1:3001", "https://budget-buddy-one-pi.vercel.app/", "https://budget-buddy.danvarela.com"
 
     resource "*",
       headers: :any,
