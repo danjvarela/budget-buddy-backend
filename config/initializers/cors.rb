93dcd355754f7ac611d7eb26ad3c5eb7180f6fd6
Dan Varela <vareladanmarj@gmail.com>
2024-07-06 06:22:31 +0800

    chore: use frontend url in credentials to determine allowed origins for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "http://127.0.0.1:3001", "https://budget-buddy-one-pi.vercel.app/", "https://budget-buddy.danvarela.com"
+    origins Rails.application.credentials.frontend[:url]
 
     resource "*",
       headers: :any,
